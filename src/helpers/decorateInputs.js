2e5ea9d560a3fff5d4d1879a90508f0bf2e14a4b
Raif Harik <reharik@gmail.com>
2017-01-01 19:08:24 -0600

    removed the key i added to try and force rerender

--- src/helpers/decorateInputs.js
+++ src/helpers/decorateInputs.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import selectn from 'selectn';
 
-const decorateInput = (children, model, shouldRerender) => {
+const decorateInput = (children, model) => {
   return React.Children.map(children, x => {
     if (!x.props) { return x; }
     const property = model[selectn('frfProperty.name', x.props)];
@@ -14,11 +14,9 @@ const decorateInput = (children, model, shouldRerender) => {
         // couple hacks here (dataVal and rerenderHack) to trigger a rerender if those props have changed
 
       return React.cloneElement(x, {
-        key: property,
         data: property,
         dataVal: property.value,
         rerenderHack: property.errors.length > 0 ? property.errors : undefined,
-        // shouldRerender: shouldRerender ? new Date().toString() : undefined
       });
     }
     let clonedItems = decorateInput(x.props.children, model);
